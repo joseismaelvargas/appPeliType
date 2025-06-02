855bcf9bf5d72ebb6bd9382564d3603c0141fa1e
ismael <joseismaelvargas@users.noreply.github.com>
2025-06-02 14:52:39 -0300

    se agrego el nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
-
+import Nav from "./common/Nav"
 
 function App() {
  
 
   return (
     <>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <Nav></Nav>
 
     </>
   )
