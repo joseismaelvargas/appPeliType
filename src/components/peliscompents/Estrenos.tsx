327ce720adf94ea3f849abd5f5cf78ca5b459796
ismael <joseismaelvargas@users.noreply.github.com>
2025-06-02 16:10:13 -0300

    se areglo

--- src/components/peliscompents/Estrenos.tsx
+++ src/components/peliscompents/Estrenos.tsx
@@ -1,5 +1,6 @@
-import { useEffect,useState,FC} from "react"
-import { Pelicula } from "../Main";
+import { useEffect,useState} from "react"
+import type { FC } from "react";
+import type { Pelicula } from "../Main";
  interface Props {
     tipos:string;
     slug:string
