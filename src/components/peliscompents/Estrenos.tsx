969d54b586e9679654bb630d5c407dc76204e2e7
ismael <joseismaelvargas@users.noreply.github.com>
2025-06-02 22:41:53 -0300

    ajustes css

--- src/components/peliscompents/Estrenos.tsx
+++ src/components/peliscompents/Estrenos.tsx
@@ -61,7 +61,10 @@ const Estrenos:FC<Props>= ({tipos,slug}) => {
             estrenos.map((item)=>
                <SwiperSlide key={item.id}>
                      <Link to={`/infopelicula/${item.id}`} className="poster">
-                         <motion.img   whileHover={{scale:1.1}}  initial="inicial" animate="animate" className="img-movie" src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt={item.title} /><p className="nunito-uniquifier-text">{item.title}</p>
+                      <motion.div whileHover={{scale:1.1}}>
+                           <motion.img  initial="inicial" animate="animate" className="img-movie" src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt={item.title} /><p className="nunito-uniquifier-text">{item.title}</p>
+                      </motion.div>
+                      
                      </Link>
                      
                </SwiperSlide>
