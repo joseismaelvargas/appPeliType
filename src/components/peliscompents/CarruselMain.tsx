d7b56aea8d11ee5e7508cf866ce7aa5d75d1b7d7
ismael <joseismaelvargas@users.noreply.github.com>
2025-07-08 22:09:50 -0300

    se genero el carrusel

--- src/components/peliscompents/CarruselMain.tsx
+++ src/components/peliscompents/CarruselMain.tsx
@@ -1,51 +1,77 @@
 import "../css/carruselMain.css"
 import { SwiperSlide,Swiper} from "swiper/react"
 import { Navigation,Autoplay } from "swiper/modules"
-
+import { useState,useEffect} from "react"
+import type { Pelicula } from "../Main"
 const CarruselMain = () => {
+  const [Carrusel,setCarrusel]=useState<Pelicula[]>([])
+  
+  const getApi=async()=>{
+                   
+                         try {  
+       
+      const api = `https://api.themoviedb.org/3/movie/now_playing?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=1`;
+                const response=await fetch(api)
+                  if(response.status===200){
+                    const data=await response.json()
+                        setCarrusel(data.results)
+                    console.log(data.results)
+                          }
+                         }catch{
+                            console.error("Error en la peticion")
+                         }
+                        
+                         
+                 }
+          useEffect(()=>{
+                 getApi()
+          },[])
+          
   return (
-    <section className="section-carru">
-        <Swiper modules={[ Navigation,Autoplay]}
-       slidesPerView={1} 
+    <section className="section-carru">  
+    <Swiper modules={[ Navigation,Autoplay]}
+         slidesPerView={1} 
        spaceBetween={30}
+       autoplay={{
+        delay:4000
+       }
+        
+       }
       
-      
-      loop={true}
-       >
-         <SwiperSlide >
-            <div className="img-degrade">
+        loop={true}
+   
+          >
+      {
+        Carrusel.map((item)=>
+       
+         <SwiperSlide    key={item.id}>
+            <div className="img-degrade-main">
 
-           <img src={"https://image.tmdb.org/t/p/w500//nSb3nNiL6pOEDK34T0U6RKTGzlJ.jpg"} alt="img" className='img-carrusel' />
+           <img
+            
+            src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+            alt=""
+          />
              </div>
             <div className='div-info'>
                 
                 <div className="text">
                 
-                    <h1>Harry Potter</h1> 
-                       <p className="hora">2h 14min 2025</p>
-                    <p>Cinco años después de Dominion, la ecología del planeta ha demostrado ser en gran medida inhóspita para los dinosaurios. Las criaturas más colosales de esa biosfera tienen la clave para un fármaco que</p> 
+                    <h1>{item.title}</h1> 
+                       <p className="hora">{item.release_date}</p>
+                    <p>{item.overview}</p> 
                 <button className="bottonver">Ver pelicula </button>
                 </div>
            
              </div> 
             
          </SwiperSlide>
-         <SwiperSlide>
-         <img src={"https://wallpapers.com/images/high/harry-potter-4k-3840-x-2160-wallpaper-w3bhjwas5ukv4ta3.webp"} alt="img" className='img-carrusel' /> 
-         <div
-             className='div-info'>
-                <h2>Entre el cielo y la tierra</h2>
-             </div>
-         </SwiperSlide>
-         <SwiperSlide>
-         <img src={"https://wallpapers.com/images/high/harry-potter-4k-3840-x-2160-wallpaper-w3bhjwas5ukv4ta3.webp"} alt="img" className='img-carrusel' /> 
-         <div
-             className='div-info'>
-                <h2>Un ambiente unico, relajado, alejado, tranquilo.</h2>
-             </div>
-         </SwiperSlide>
-       </Swiper>
+        
       
+      
+        )
+      }
+        </Swiper>
     </section>
 
   )
